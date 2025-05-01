f4de4742f6c7007c2dc6269a9c5f6b004cc68638
ZahaAnass <zahaanass277@gmail.com>
2025-05-02 00:40:11 +0100

    add the database connection file

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$dbname = 'zoomix_rental';
+$username = 'root';
+$password = ''; 
+$charset = 'utf8mb4';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $username, $password);
+    // Enable errors
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Set Default Fetch Mode
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // Test Connection
+    echo "DB Connection successful";
+} catch (PDOException $e) {
+    die("DB Connection failed: " . $e->getMessage());
+}
+?>
