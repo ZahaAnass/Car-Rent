eea0c9162a3cda8394adc19fa0505990b59c98a5
ZahaAnass <zahaanass277@gmail.com>
2025-05-02 23:56:10 +0100

    enhance content for service page

--- public/service.php
+++ public/service.php
@@ -26,8 +26,7 @@
             <div class="container py-5">
                 <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 800px;">
                     <h1 class="display-5 text-capitalize mb-3">Zoomix <span class="text-primary">Services</span></h1>
-                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet nemo expedita asperiores commodi accusantium at cum harum, excepturi, quia tempora cupiditate! Adipisci facilis modi quisquam quia distinctio,
-                    </p>
+                    <p class="mb-0">Discover a world of convenience and flexibility with our comprehensive car rental services. We're committed to making your journey smooth, affordable, and tailored to your unique needs.</p>
                 </div>
                 <div class="row g-4">
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -35,8 +34,8 @@
                             <div class="service-icon mb-4">
                                 <i class="fa fa-phone-alt fa-2x"></i>
                             </div>
-                            <h5 class="mb-3">Phone Reservation</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam quasi quibusdam ipsa perferendis iusto?</p>
+                            <h5 class="mb-3">Easy Phone Reservation</h5>
+                            <p class="mb-0">Simplify your booking with our hassle-free phone reservation service. Our friendly representatives are ready to help you find the perfect vehicle and provide personalized assistance.</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
@@ -44,8 +43,8 @@
                             <div class="service-icon mb-4">
                                 <i class="fa fa-money-bill-alt fa-2x"></i>
                             </div>
-                            <h5 class="mb-3">Special Rates</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam quasi quibusdam ipsa perferendis iusto?</p>
+                            <h5 class="mb-3">Competitive Rates</h5>
+                            <p class="mb-0">Enjoy unbeatable pricing with our special rates. We offer transparent, budget-friendly options without compromising on quality, ensuring the best value for your rental.</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
@@ -53,8 +52,8 @@
                             <div class="service-icon mb-4">
                                 <i class="fa fa-road fa-2x"></i>
                             </div>
-                            <h5 class="mb-3">One Way Rental</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam quasi quibusdam ipsa perferendis iusto?</p>
+                            <h5 class="mb-3">Flexible One-Way Rental</h5>
+                            <p class="mb-0">Travel with ultimate freedom through our one-way rental service. Pick up and drop off your vehicle at different locations, maximizing convenience and flexibility.</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
@@ -62,8 +61,8 @@
                             <div class="service-icon mb-4">
                                 <i class="fa fa-umbrella fa-2x"></i>
                             </div>
-                            <h5 class="mb-3">Life Insurance</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam quasi quibusdam ipsa perferendis iusto?</p>
+                            <h5 class="mb-3">Comprehensive Protection</h5>
+                            <p class="mb-0">Drive with complete peace of mind through our comprehensive insurance coverage. We protect you throughout your journey, ensuring confidence and security.</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
@@ -71,8 +70,8 @@
                             <div class="service-icon mb-4">
                                 <i class="fa fa-building fa-2x"></i>
                             </div>
-                            <h5 class="mb-3">City to City</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam quasi quibusdam ipsa perferendis iusto?</p>
+                            <h5 class="mb-3">City-to-City Travel</h5>
+                            <p class="mb-0">Explore beyond city limits with our seamless city-to-city rental service. Experience the freedom to travel between destinations with ease and unparalleled convenience.</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
@@ -80,8 +79,8 @@
                             <div class="service-icon mb-4">
                                 <i class="fa fa-car-alt fa-2x"></i>
                             </div>
-                            <h5 class="mb-3">Free Rides</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam quasi quibusdam ipsa perferendis iusto?</p>
+                            <h5 class="mb-3">Welcome Perks</h5>
+                            <p class="mb-0">Enjoy our special welcome offers designed to enhance your rental experience. From complimentary upgrades to bonus miles, we go the extra mile for you.</p>
                         </div>
                     </div>
                 </div>
@@ -150,72 +149,71 @@
         <div class="container-fluid testimonial py-5">
             <div class="container py-5">
                 <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 800px;">
-                    <h1 class="display-5 text-capitalize mb-3">Our Clients<span class="text-primary"> Riviews</span></h1>
-                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet nemo expedita asperiores commodi accusantium at cum harum, excepturi, quia tempora cupiditate! Adipisci facilis modi quisquam quia distinctio,
-                    </p>
+                    <h1 class="display-5 text-capitalize mb-3">Our Clients<span class="text-primary"> Reviews</span></h1>
+                    <p class="mb-0">Hear directly from our satisfied customers who have experienced the Zoomix difference. Their stories showcase our commitment to exceptional service and unforgettable travel experiences.</p>
                 </div>
                 <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
                     <div class="testimonial-item">
                         <div class="testimonial-quote"><i class="fa fa-quote-right fa-2x"></i>
                         </div>
                         <div class="testimonial-inner p-4">
-                            <img src="img/testimonial-1.jpg" class="img-fluid" alt="">
+                            <img src="../assets/img/testimonial-1.jpg" class="img-fluid" alt="David Martinez, Business Traveler">
                             <div class="ms-4">
-                                <h4>Person Name</h4>
-                                <p>Profession</p>
+                                <h4>David Martinez</h4>
+                                <p>Business Consultant</p>
                                 <div class="d-flex text-primary">
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
-                                    <i class="fas fa-star text-body"></i>
+                                    <i class="fas fa-star"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="border-top rounded-bottom p-4">
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta neque ab repudiandae reprehenderit ipsum eos cumque esse repellendus impedit.</p>
+                            <p class="mb-0">"Zoomix has been my go-to car rental service for business trips. Their fleet is always clean, reliable, and perfectly maintained. The booking process is seamless, and their customer support is exceptional."</p>
                         </div>
                     </div>
                     <div class="testimonial-item">
                         <div class="testimonial-quote"><i class="fa fa-quote-right fa-2x"></i>
                         </div>
                         <div class="testimonial-inner p-4">
-                            <img src="img/testimonial-2.jpg" class="img-fluid" alt="">
+                            <img src="../assets/img/testimonial-2.jpg" class="img-fluid" alt="Emily Chen, Travel Enthusiast">
                             <div class="ms-4">
-                                <h4>Person Name</h4>
-                                <p>Profession</p>
+                                <h4>Emily Chen</h4>
+                                <p>Travel Blogger</p>
                                 <div class="d-flex text-primary">
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
-                                    <i class="fas fa-star text-body"></i>
+                                    <i class="fas fa-star"></i>
                                     <i class="fas fa-star text-body"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="border-top rounded-bottom p-4">
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta neque ab repudiandae reprehenderit ipsum eos cumque esse repellendus impedit.</p>
+                            <p class="mb-0">"As a travel blogger, I've rented cars from many companies, but Zoomix stands out. Their diverse vehicle selection, competitive prices, and friendly service make every trip memorable. Highly recommended!"</p>
                         </div>
                     </div>
                     <div class="testimonial-item">
                         <div class="testimonial-quote"><i class="fa fa-quote-right fa-2x"></i>
                         </div>
                         <div class="testimonial-inner p-4">
-                            <img src="img/testimonial-3.jpg" class="img-fluid" alt="">
+                            <img src="../assets/img/testimonial-3.jpg" class="img-fluid" alt="Michael Rodriguez, Family Traveler">
                             <div class="ms-4">
-                                <h4>Person Name</h4>
-                                <p>Profession</p>
+                                <h4>Michael Rodriguez</h4>
+                                <p>Family Vacationer</p>
                                 <div class="d-flex text-primary">
                                     <i class="fas fa-star"></i>
                                     <i class="fas fa-star"></i>
-                                    <i class="fas fa-star text-body"></i>
+                                    <i class="fas fa-star"></i>
                                     <i class="fas fa-star text-body"></i>
                                     <i class="fas fa-star text-body"></i>
                                 </div>
                             </div>
                         </div>
                         <div class="border-top rounded-bottom p-4">
-                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta neque ab repudiandae reprehenderit ipsum eos cumque esse repellendus impedit.</p>
+                            <p class="mb-0">"Planning a family road trip can be stressful, but Zoomix made it easy. They helped us find the perfect SUV for our needs, and the whole process was smooth from start to finish. Great service!"</p>
                         </div>
                     </div>
                 </div>
@@ -224,17 +222,21 @@
         <!-- Testimonial End -->
 
         <!-- Banner Start -->
-        <div class="container-fluid banner pb-5 wow zoomInDown" data-wow-delay="0.1s">
-            <div class="container pb-5">
+        <div class="container-fluid banner py-5 wow zoomInDown" data-wow-delay="0.1s">
+            <div class="container py-5">
                 <div class="banner-item rounded">
-                    <img src="img/banner-1.jpg" class="img-fluid rounded w-100" alt="">
+                    <img src="../assets/img/banner-1.jpg" class="img-fluid rounded w-100" alt="Car Rental Banner">
                     <div class="banner-content">
-                        <h2 class="text-primary">Rent Your Car</h2>
-                        <h1 class="text-white">Interested in Renting?</h1>
-                        <p class="text-white">Don't hesitate and send us a message.</p>
+                        <h2 class="text-primary">Your Journey Starts Here</h2>
+                        <h1 class="text-white">Ready to Hit the Road?</h1>
+                        <p class="text-white">Discover the perfect vehicle for your next adventure. Quick, easy, and hassle-free rentals.</p>
                         <div class="banner-btn">
-                            <a href="#" class="btn btn-secondary rounded-pill py-3 px-4 px-md-5 me-2">WhatchApp</a>
-                            <a href="#" class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2">Contact Us</a>
+                            <a href="#" class="btn btn-secondary rounded-pill py-3 px-4 px-md-5 me-2">
+                                <i class="fab fa-whatsapp me-2"></i>WhatsApp
+                            </a>
+                            <a href="contact.php" class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2">
+                                <i class="fas fa-envelope me-2"></i>Contact Us
+                            </a>
                         </div>
                     </div>
                 </div>
