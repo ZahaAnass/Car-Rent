dc17a7c1b1cb6bf9f5478cb72478b55f9355743b
ZahaAnass <zahaanass277@gmail.com>
2025-05-05 23:06:30 +0100

    add some adjustement to bottom-navar

--- includes/bottom-nav.php
+++ includes/bottom-nav.php
@@ -2,25 +2,31 @@
     <div class="container-fluid">
         <div class="row text-center bg-dark py-2 shadow-lg">
             <div class="col">
-                <a href="dashbord.php" class="text-white <?= basename($_SERVER['PHP_SELF']) == 'dashbord.php' ? 'text-primary' : '' ?>">
+                <a href="../public/index.php" class="text-white">
                     <i class="fas fa-home"></i>
                     <small class="d-block">Home</small>
                 </a>
             </div>
             <div class="col">
-                <a href="book-car.php" class="text-white <?= basename($_SERVER['PHP_SELF']) == 'book-car.php' ? 'text-primary' : '' ?>">
+                <a href="dashbord.php" class="<?= basename($_SERVER['PHP_SELF']) == 'dashbord.php' ? 'text-primary' : 'text-white' ?>">
+                    <i class="fas fa-user"></i>
+                    <small class="d-block">Dashboard</small>
+                </a>
+            </div>
+            <div class="col">
+                <a href="book-car.php" class="<?= basename($_SERVER['PHP_SELF']) == 'book-car.php' ? 'text-primary' : 'text-white' ?>">
                     <i class="fas fa-car"></i>
                     <small class="d-block">Book</small>
                 </a>
             </div>
             <div class="col">
-                <a href="my-reservations.php" class="text-white <?= basename($_SERVER['PHP_SELF']) == 'my-reservations.php' ? 'text-primary' : '' ?>">
+                <a href="my-reservations.php" class="<?= basename($_SERVER['PHP_SELF']) == 'my-reservations.php' ? 'text-primary' : 'text-white' ?>">
                     <i class="fas fa-calendar-alt"></i>
                     <small class="d-block">Reservations</small>
                 </a>
             </div>
             <div class="col">
-                <a href="settings.php" class="text-white <?= basename($_SERVER['PHP_SELF']) == 'settings.php' ? 'text-primary' : '' ?>">
+                <a href="settings.php" class="<?= basename($_SERVER['PHP_SELF']) == 'settings.php' ? 'text-primary' : 'text-white' ?>">
                     <i class="fas fa-cog"></i>
                     <small class="d-block">Settings</small>
                 </a>
