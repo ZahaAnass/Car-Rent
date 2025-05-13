8121d910d408c747c4b3e904b042bb6227fbbc7c
ZahaAnass <zahaanass277@gmail.com>
2025-05-13 14:04:45 +0100

    feat: Add admin authentication check

---
+++ includes/auth_admin_check.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once '../includes/session.php';
+start_session();
+
+include_once '../includes/functions.php';
+
+// Check If User Is Logged In
+if (!is_logged_in()) {
+    redirect('../public/auth/login.php');
+}
+
+// Check If User Isn't Admin
+if (!is_admin()) {
+    redirect('../user/dashboard.php');
+}
