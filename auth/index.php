b9b0b9b5bde6c8153e1aba15f5bb9e7e6bbb761c
ZahaAnass <zahaanass277@gmail.com>
2025-05-18 17:29:57 +0100

    add the index page that transfer to home page

---
+++ auth/index.php
@@ -0,0 +1,13 @@
+<?php
+    require_once '../includes/session.php';
+    start_session();
+
+    require_once '../includes/functions.php';
+    
+    if (is_logged_in() && is_admin()) {
+        redirect('../public/index.php');
+    }elseif (is_logged_in() && !is_admin()) {
+        redirect('../public/index.php');
+    }
+
+?>
