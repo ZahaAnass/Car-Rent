99ea7745c978e3857ab6075abb53684ca697e258
ZahaAnass <zahaanass277@gmail.com>
2025-05-20 01:21:29 +0100

    fix validate functions and update logics

--- user/book-car-handeler.php
+++ user/book-car-handeler.php
@@ -10,7 +10,7 @@
     $car = $carQueries->getCarById($selected_car_id);
     $user_id = $_SESSION['user_id'];
     if(!$car || ( $car['status'] !== 'Available' && $car['status'] !== 'Rented' )) {
-        $_SESSION['error'] = "Car not found.";
+        $_SESSION['booking_error'] = "Car not found.";
         redirect("../public/cars.php");
     }
 
@@ -19,33 +19,33 @@
         $return_date = trim(filter_input(INPUT_POST, 'return_date', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
         $pickup_location = trim(filter_input(INPUT_POST, 'pickup_location', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
         $return_location = trim(filter_input(INPUT_POST, 'return_location', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+        $daily_rate = trim(filter_input(INPUT_POST, 'daily_rate', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
         $total_price = trim(filter_input(INPUT_POST, 'total_price', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
 
         if(!$pickup_date || !$return_date) {
-            $_SESSION['error'] = "Invalid date format. Please use YYYY-MM-DD.";
-            redirect("../public/book-car.php?car_id=" . $selected_car_id);
+            $_SESSION['booking_error'] = "Invalid date format. Please use YYYY-MM-DD.";
+            redirect("../user/book-car.php?car_id=" . $selected_car_id);
         }elseif(!validate_date($pickup_date) || !validate_date($return_date)){
-            $_SESSION['error'] = "Invalid date format. Please use YYYY-MM-DD.";
-            redirect("../public/book-car.php?car_id=" . $selected_car_id);
-        }
-        elseif($pickup_date >= $return_date) {
-            $_SESSION['error'] = "Pickup date must be before return date.";
-            redirect("../public/book-car.php?car_id=" . $selected_car_id);
+            $_SESSION['booking_error'] = "Invalid date format. Please use YYYY-MM-DD.";
+            redirect("../user/book-car.php?car_id=" . $selected_car_id);
+        }elseif($pickup_date >= $return_date) {
+            $_SESSION['booking_error'] = "Pickup date must be before return date.";
+            redirect("../user/book-car.php?car_id=" . $selected_car_id);
         }elseif(!$total_price) {
-            $_SESSION['error'] = "Invalid price format. Please use a valid price.";
-            redirect("../public/book-car.php?car_id=" . $selected_car_id);
+            $_SESSION['booking_error'] = "Invalid price format. Please use a valid price.";
+            redirect("../user/book-car.php?car_id=" . $selected_car_id);
         }else{
             try {
                 $bookingQueries = new BookingQueries($pdo);
                 $isAvailable = $bookingQueries->checkCarAvailability($selected_car_id, $pickup_date, $return_date);
                 if(!$isAvailable) {
-                    $_SESSION['error'] = "Car is not available for the selected dates.";
-                    redirect("../public/book-car.php?car_id=" . $selected_car_id);
+                    $_SESSION['booking_error'] = "Car is not available for the selected dates.";
+                    redirect("../user/book-car.php?car_id=" . $selected_car_id);
                 }
                 $result = $bookingQueries->createBooking($user_id, $selected_car_id, $pickup_date, $return_date, $pickup_location, $return_location, $total_price);
                 if ($result) {
-                    $_SESSION['register_success'] = 'Booking added successfully!';
-                    redirect('../public/book-car.php?car_id=' . $selected_car_id);
+                    $_SESSION['booking_success'] = 'Booking added successfully!';
+                    redirect('../user/book-car.php?car_id=' . $selected_car_id);
                 } else {
                     $error = 'Add failed. Please try again.';
                 }
@@ -55,8 +55,8 @@
         }
     }
     if (!empty($error)) {
-        $_SESSION['register_error'] = $error;
-        redirect('../public/book-car.php?car_id=' . $selected_car_id);
+        $_SESSION['booking_error'] = $error;
+        redirect('../user/book-car.php?car_id=' . $selected_car_id);
     }
 
 ?>
