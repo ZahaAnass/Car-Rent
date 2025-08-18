3341da57737ef2d0386213ec9e5a842d2d1b1257
ZahaAnass <zahaanass277@gmail.com>
2025-08-18 16:19:41 +0100

    fix the session error and success message

--- public/contact.php
+++ public/contact.php
@@ -64,7 +64,7 @@
                 <?= htmlspecialchars($_SESSION['success']) ?>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        <?php endif; ?>
+        <?php unset($_SESSION['success']); endif; ?>
 
 
         <!-- Header Start -->
